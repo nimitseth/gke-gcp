559129b0cb0b63dbdca7492ab0f58ad12d6b46d8
Vijay K <sharmavijay86@gmail.com>
2022-11-19 21:40:56 +0530

    Update providers.tf

--- labstart/providers.tf
+++ labstart/providers.tf
@@ -1,12 +1,5 @@
 terraform {
   backend "http" {
-    address = local.backend.gitlab_url
-    lock_address = "${local.backend.gitlab_url}/lock"
-    unlock_address = "${local.backend.gitlab_url}/lock"
-    username = local.backend.gitlab_user
-    password = local.backend.gitlab_url
-    lock_method    = "POST"
-    unlock_method  = "DELETE"
   }
   required_providers {
     google = {
@@ -15,13 +8,7 @@ terraform {
     }
   }
 }
-locals {
-  backend = {
-    gitlab_url = var.gitlab_url
-    gitlab_user = var.gitlab_user
-    gitlab_token = var.gitlab_token
-}
-}
+
 provider "random" {
 }
 
