1979d50de0793dfb6ffbaa38ba9f24d353354d88
Nimit Seth <nimitseth@gmail.com>
2023-02-21 19:05:21 +0530

    Update provider.tf

--- gke/provider.tf
+++ gke/provider.tf
@@ -1,6 +1,9 @@
 terraform {
-  backend "http" {
+  backend "gcs" {
+    bucket  = "tf-state-prod21"
+    prefix  = "terraform/state"
   }
+  
   required_providers {
     google = {
       source  = "hashicorp/google"
